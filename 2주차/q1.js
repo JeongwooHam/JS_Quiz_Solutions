ab4b565fe63331d56f2de73aba86170bb033efe3
jeongwooHam <hamjw0122@gmail.com>
2023-04-08 00:47:22 +0900

    최종

--- 2주차/q1.js
+++ 2주차/q1.js
@@ -1,7 +1,7 @@
 /*
 변수 위치 찾기
 [O]변수 a는 main 함수 안에서 사용된다
-[]변수 b는 dom api selector로 다른 이벤트에서 사용된다
+[?]변수 b는 dom api selector로 다른 이벤트에서 사용된다
 [O]변수 c는 메인 함수 안에서 실행되는 콜백함수 solution의 인자이다
 [O]변수 d는 솔루션 함수에서 전달받은 c를 다른 형태로 바꿔준다.
 [O]변수 e는 main함수의 최종 반환 값으로 향후 다른 함수에서 재사용된다.
@@ -13,15 +13,15 @@ function solution(c) {
 }
 
 function parse() {
-  let e = document.querySelector("something");
-  e.addEventListener("click", function () {
-    e.style.display = "none";
+  let b = document.querySelector("something");
+  b.addEventListener("click", function () {
+    b.style.display = "none";
   });
 }
 
-function main(solution) {
+function main(action) {
   let a = 10;
-  let e = solution + a;
+  let e = action + a;
   return e;
 }
 
--- 2주차/q2.js
+++ 2주차/q2.js
@@ -12,18 +12,16 @@ ex) 길이가 80인 배열을 '길이가 5인 배열들'로 분해한다면 16�
    2 [11, 12, 13, 14 ,15] 2*5
    3 [16, 17, 18, 19, 20] 3*5 */
 
-const arr = [
-  1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
-];
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
 function division(num) {
   let result = [];
-  for (let i = 0; i < Math.floor(arr.length / num); i++) {
+  for (let i = 0; i < Math.ceil(arr.length / num); i++) {
     result.push(arr.slice(i * num, i * num + num));
   }
   return result;
 }
 
-console.log(division(3));
+console.log(division(2));
 
 // Array.from 사용해서 다시 풀어보기
